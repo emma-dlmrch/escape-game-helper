02d3066094a00dcdbbe6ad2742cc4175dade9d90
emma-dlmrch <emma.delamarche@gmail.com>
2023-11-01 21:03:32 +0100

    Add static

--- escape-game-api/escapegameapi/escapegameapi/settings.py
+++ escape-game-api/escapegameapi/escapegameapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 import logging
 from pathlib import Path
+import os
 
 from .local_settings import *
 
@@ -123,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
